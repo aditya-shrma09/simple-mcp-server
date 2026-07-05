5f014c056e2a538d44de9fc4e01534c4fe380302
aditya-shrma09 <aditya.sharma.9.11.2004@gmail.com>
2026-07-06 00:54:17 +0530

    client server connection established

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+host = "127.0.0.1"
+port = 5000
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect((host,port))
+
+client.sendall(b"hello server")
+
+resp = client.recv(1024)
+
+print(resp.decode())
+
+client.close
--- server.py
+++ server.py
@@ -1,20 +1,53 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp =  FastMCP("calculator")
+import socket
+host = "127.0.0.1"
+port = 5000
 
-def add(a:int,b:int)->int:
-    return int(a)+int(b)
-def subtract(a:int,b:int)->int:
-    return int(a)-int(b)
-def multiply(a:int,b:int)->int:
-    return int(a)*int(b)
-def divide(a:int,b:int):
-    return int(a)/int(b)
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen()
+print("Server is listening...")
 
-mcp.tool()(add)
-mcp.tool()(multiply)
+conn, addr = s.accept()
 
-mcp.tool()(subtract)
-mcp.tool()(divide)
-if __name__ == "__main__":
-    mcp.run()
+print("Connected by", addr)
+
+data = conn.recv(1024)
+print("Received:", data.decode())
+
+conn.sendall(b"Hello Client!")
+
+
+
+# mcp =  FastMCP("calculator")
+
+# def add(a:int,b:int)->int:
+#     """Add two integers."""
+#     return int(a)+int(b)
+# def subtract(a:int,b:int)->int:
+#     """subtract two integers."""
+#     return int(a)-int(b)
+# def multiply(a:int,b:int)->int:
+#     """multiply two integers."""
+#     return int(a)*int(b)
+# def divide(a:int,b:int):
+#     """divide two integers."""
+#     if b==0:
+#         raise ValueError("b cannot be 0")
+#     return int(a)/int(b)
+# def power(a:int,b:int)->int:
+#     """to use when raising one integer to power of another integer"""
+#     return a**b
+
+# mcp.tool()(power)
+# mcp.tool()(add)
+# mcp.tool()(multiply)
+
+# mcp.tool()(subtract)
+# mcp.tool()(divide)
+# if __name__ != "__main__":
+#     mcp.run()
+
+conn.close()
+s.close()
